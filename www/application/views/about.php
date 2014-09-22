c3a09c0599b6919da901767aa74ee7b05c618e78
Thomas Di'Meco <thomas.dimeco@gmail.com>
2014-09-23 00:32:15 +0200

    Added TouchRAM and Smart Campus projects

--- www/application/views/about.php
+++ www/application/views/about.php
@@ -8,11 +8,11 @@
 <p>Dans ce vaste domaine qu'est l'informatique, ce qui m'intéresse particulièrement, c'est le développement en général (Java, C++, C...), les technologies web (PHP, HTML, CSS, JavaScript...), et je commence à m'intéresser aux technologies mobiles, notamment iOS (Objective-C/Cocoa). J'ai également eu la chance d'avoir une excellente formation en bases de données à l'IUT d'Aix-en-Provence (Oracle, mais aussi MySQL, PostgreSQL et SQLite). Plus généralement, je suis un utilisateur de OS&nbsp;X, j'ai l'habitude d'administrer des serveurs sous Debian et j'utilise régulièrement l'incontournable Windows.</p>
 <p class="space-before">Plus récemment, j'ai découvert un domaine qui m'intéresse de plus en plus : l'ingénierie des modèles, ou MDE (model-driven engineering).</p>
 <p class="space-before">Pour en savoir un peu plus sur mes compétences et mes expériences, je vous invite à visiter mon profil LinkedIn :</p>
-<p id="about-more"><a href="https://www.linkedin.com/pub/thomas-di-meco/60/329/500" class="button">Profil LinkedIn</a></p>
+<p id="about-linkedin"><a href="https://www.linkedin.com/pub/thomas-di-meco/60/329/500" class="button">Profil LinkedIn</a></p>
 <h2>Photographie</h2>
 <p>Récemment, j'ai acheté mon premier appareil photo hybride à objectifs interchangeables (<a href="http://www.fujifilm.eu/fr/produits/appareils-photo-numeriques/appareils-a-objectif-interchangeable/model/x-m1/">Fujifilm X-M1</a>) que j'affectionne particulièrement et je me suis découvert une passion pour la photographie. Je suis actuellement en train d'apprendre et si vous avez des conseils, c'est avec plaisir que je les saisirais !</p>
 <p class="space-before">J'ai créé une galerie Flickr dans le but de diffuser les quelques photos dont je suis fier, n'hésitez pas à aller y faire un tour et à me donner des conseils pour progresser :</p>
-<p id="about-more"><a href="https://www.flickr.com/photos/tdimeco/" class="button">Galerie Flickr</a></p>
+<p id="about-flickr"><a href="https://www.flickr.com/photos/tdimeco/" class="button">Galerie Flickr</a></p>
 <h2 id="contact">Me contacter</h2>
 <p>Je reste à votre disposition si vous souhaitez me contacter, que ce soit pour en savoir davantage ou simplement pour poser une question.</p>
 <p id="about-contact"><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64;&#116;&#100;&#105;&#109;&#101;&#99;&#111;&#46;&#102;&#114;&#63;&#115;&#117;&#98;&#106;&#101;&#99;&#116;&#61;&#91;&#116;&#100;&#105;&#109;&#101;&#99;&#111;&#46;&#102;&#114;&#93;" class="button">Me contacter par e-mail</a></p>
