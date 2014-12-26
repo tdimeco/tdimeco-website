c64ed4c108973f7f44e4513877d0ed20ae0c3dbd
Thomas Di'Meco <thomas.dimeco@gmail.com>
2014-12-26 18:01:29 +0100

    Basic HTML 5 structure

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name tdimecoApp.controller:MainCtrl
+ * @name tdimecoApp.controller:HomeCtrl
  * @description
- * # MainCtrl
+ * # HomeCtrl
  * Controller of the tdimecoApp
  */
 angular.module('tdimecoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
