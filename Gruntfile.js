20ed1a4919a4a9fe7d41b7d512e2daea68b52864
Thomas Di'Meco <thomas.dimeco@gmail.com>
2015-03-16 19:39:08 +0100

    Fixed issue with icons filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -189,6 +189,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '!<%= yeoman.dist %>/images/icons/*',
           '<%= yeoman.dist %>/fonts/*'
         ]
       }
