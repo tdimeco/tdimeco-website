d6668f8f4256a04229ad4050659788f6293735a0
Thomas Di Meco <thomas.dimeco@gmail.com>
2017-08-31 11:31:21 +0200

    Apps page

---
+++ src/components/apps-page/apps-page.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <page-header title="Apps" subtitle="Mes principales applications iOS / macOS / watchOS"></page-header>
+    <div class="container">
+      <div class="grid row">
+        <div class="grid-sizer col-xs-12 col-sm-6 col-md-4"></div>
+        <div v-for="app in apps" class="grid-item col-xs-12 col-sm-6 col-md-4">
+          <card-item :item="app"></card-item>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+var Masonry = require('masonry-layout')
+import Apps from '../../data/apps'
+import PageHeader from '../page-header/page-header.vue'
+import CardItem from '../card-item/card-item.vue'
+export default {
+  components: {PageHeader, CardItem},
+  data: function () {
+    return {
+      apps: Apps
+    }
+  },
+  mounted: function () {
+    new Masonry('.grid', {
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      transitionDuration: 0
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../styles/variables-mixins.less';
+@grid-padding: @grid-gutter-width / 2;
+.grid {
+  padding: @grid-padding 0;
+  .grid-item {
+    padding: @grid-padding;
+  }
+  @media (max-width: @screen-xs-max) {
+    padding: 0 0 @grid-padding 0;
+    .grid-item {
+      padding: @grid-padding @grid-padding 0 @grid-padding;
+    }
+  }
+}
+</style>
---
+++ src/components/card-item/card-item.vue
@@ -0,0 +1,94 @@
+<template>
+  <section>
+    <header :style="'background-color:' + item.color">
+      <div class="content">
+        <div class="column">
+          <img :src="item.icon" alt="">
+        </div>
+        <div class="column">
+          <div class="title">{{item.title}}</div>
+          <div class="subtitle">{{item.subtitle}}</div>
+        </div>
+      </div>
+    </header>
+    <div>
+      <div v-html="item.description"></div>
+    </div>
+    <footer>
+      <ul>
+        <li v-for="link in item.links">
+          <a :href="link.url" :class="{secondary: link.secondary === true}">
+            <span :class="typeof link.iconClasses === 'string' ? link.iconClasses : 'fa fa-link'"></span>
+            {{link.label}}
+          </a>
+        </li>
+      </ul>
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
+
+<style lang="less" scoped>
+section {
+  border-radius: 6px;
+  overflow: hidden;
+  header {
+    padding: 15px;
+    .content {
+      display: table-row;
+      .column {
+        display: table-cell;
+        vertical-align: middle;
+        &:last-child {
+          padding-left: 15px;
+        }
+      }
+      .title {
+        font-size: 1.3em;
+        font-weight: 600;
+        color: white;
+      }
+      img {
+        width: 2.8em;
+        height: 2.8em;
+        overflow: hidden;
+        border-radius: 10px;
+      }
+    }
+  }
+  > div {
+    padding: 15px;
+    font-size: 0.9em;
+    text-align: justify;
+    background: #eeeeee;
+  }
+  footer {
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      a {
+        display: block;
+        margin-top: 3px;
+        padding: 12px 15px;
+        background: #eeeeee;
+        font-weight: 600;
+        &:hover, &:focus {
+          background: #dddddd;
+        }
+        &.secondary {
+          font-weight: inherit;
+        }
+        span {
+          margin-right: 8px;
+        }
+      }
+    }
+  }
+}
+</style>
